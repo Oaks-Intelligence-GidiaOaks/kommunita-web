b45b5ce5bdd6ae43885d092d05ef2b6b225af306
Agbugba David C <dagbugba@yahoo.com>
2024-04-02 15:59:08 +0100

    stories overflow scroll

--- src/components/main/Story.jsx
+++ src/components/main/Story.jsx
@@ -4,54 +4,211 @@ import story1 from "../../assets/images/main/story1.svg";
 import story2 from "../../assets/images/main/story2.svg";
 import story3 from "../../assets/images/main/story3.svg";
 import story4 from "../../assets/images/main/story4.svg";
+import ModalImage from "react-modal-image";
 
 function Story() {
   return (
-    <div className="stories flex justify-start gap-2 items-center w-full">
-      <div className="go-live flex flex-col justify-center items-center">
-        <p className="go-live-text pb-1">Go live</p>
-        <button className="w-[26.753px] h-[26.753px]">
-          <img src={liveBtn} alt="liveBtn" />
-        </button>
-      </div>
-      <div className="story-cards relative">
-        <img src={story1} alt="story" className="w-[91.276px] h-[110px]" />
-        <div className="absolute top-0 right-0 z-20 mr-1 -mt-3">
-          <img src={liveText} />
+    <div className="stories-container overflow-x-auto whitespace-nowrap w-full flex">
+      <div className="stories flex justify-start gap-2 items-center">
+        <div className="go-live flex flex-col justify-center items-center">
+          <p className="go-live-text pb-1">Go live</p>
+          <button className="w-[26.753px] h-[26.753px]">
+            <img src={liveBtn} alt="liveBtn" />
+          </button>
         </div>
-        <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
-          Sherrilyn Kenyon
-        </p>
-      </div>
+        <div className="story-cards relative">
+          <ModalImage
+            small={story1}
+            large={story1}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />{" "}
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Sherrilyn Kenyon
+          </p>
+        </div>
 
-      <div className="story-cards relative">
-        <img src={story2} alt="story" className="w-[91.276px] h-[110px]" />
-        <div className="absolute top-0 right-0 z-20 mr-1 -mt-3">
-          <img src={liveText} />
+        <div className="story-cards relative">
+          <ModalImage
+            small={story2}
+            large={story2}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />{" "}
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Edgar Poe
+          </p>
         </div>
-        <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
-          Edgar Poe
-        </p>
-      </div>
 
-      <div className="story-cards relative">
-        <img src={story3} alt="story" className="w-[91.276px] h-[110px]" />
-        <div className="absolute top-0 right-0 z-20 mr-1 -mt-3">
-          <img src={liveText} />
+        <div className="story-cards relative">
+          <ModalImage
+            small={story3}
+            large={story3}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />{" "}
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Nora Roberts
+          </p>
         </div>
-        <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
-          Nora Roberts
-        </p>
-      </div>
 
-      <div className="story-cards relative">
-        <img src={story4} alt="story" className="w-[91.276px] h-[110px]" />
-        <div className="absolute top-0 right-0 z-20 mr-1 -mt-3">
-          <img src={liveText} />
+        <div className="story-cards relative">
+          <ModalImage
+            small={story4}
+            large={story4}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />{" "}
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Nora Roberts
+          </p>
         </div>
-        <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
-          Charles Dickens
-        </p>
+
+        <div className="story-cards relative">
+          <ModalImage
+            small={story4}
+            large={story4}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />{" "}
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Nora Roberts
+          </p>
+        </div>
+
+        <div className="story-cards relative">
+          <ModalImage
+            small={story1}
+            large={story1}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />{" "}
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Nora Roberts
+          </p>
+        </div>
+        <div className="story-cards relative">
+          <ModalImage
+            small={story4}
+            large={story4}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />{" "}
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Nora Roberts
+          </p>
+        </div>
+
+        <div className="story-cards relative">
+          <ModalImage
+            small={story1}
+            large={story1}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />{" "}
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Charles Dickens
+          </p>
+        </div>
+
+        <div className="story-cards relative">
+          <ModalImage
+            small={story4}
+            large={story4}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />{" "}
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Charles Dickens
+          </p>
+        </div>
+
+        <div className="story-cards relative">
+          <ModalImage
+            small={story1}
+            large={story1}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />{" "}
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Charles Dickens
+          </p>
+        </div>
+
+        <div className="story-cards relative">
+          <ModalImage
+            small={story4}
+            large={story4}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />{" "}
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Charles Dickens
+          </p>
+        </div>
+
+        <div className="story-cards relative">
+          <ModalImage
+            small={story1}
+            large={story1}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Charles Dickens
+          </p>
+        </div>
+
+        <div className="story-cards relative">
+          <ModalImage
+            small={story4}
+            large={story4}
+            alt="story"
+            className="w-[91.276px] h-[110px]"
+          />
+          <div className="absolute top-1 right-0 z-20 mr-1 -mt-3">
+            <img src={liveText} />
+          </div>
+          <p className="story-name absolute bottom-0 left-0 z-20 ml-2 mb-1">
+            Charles Dickens
+          </p>
+        </div>
       </div>
     </div>
   );
