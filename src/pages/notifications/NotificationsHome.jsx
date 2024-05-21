9555ab05840b0af33d8224eb94d48b99d4933809
JosephIgbaji <igbajijoseph.k@gmail.com>
2024-05-21 16:38:26 +0100

    notifications pages updated

--- src/pages/notifications/NotificationsHome.jsx
+++ src/pages/notifications/NotificationsHome.jsx
@@ -30,67 +30,67 @@ const NotificationsHome = () => {
         </div>
       ) : (
         ntf?.map((ntf, id) => (
-          // <div
-          //   key={id}
-          //   className="flex gap-3 items-start bg-white border-b-2 p-5 w-full mt-4"
-          // >
-          //   <div
-          //     className={`rounded-full border-4 w-[40px] h-[40px]`}
-          //     // style={{ borderColor: badgeColor || {post.user_id?.department[0]?.badge?.color} }}
-          //     style={{ borderColor: "gray" }}
-          //   >
-          //     <img
-          //       src={ntf?.initiator_id?.photo_url || avatar4}
-          //       className="rounded-full w-full h-full object-cover"
-          //       alt=""
-          //     />
-          //   </div>
-          //   <div className="flex-1">
-          //     {/* <div className="flex gap-2">
-          //       <p className="post-name pb-1">{fullname || "Eko Rugged"}</p>{" "}
-          //       {verifiedUser && (
-          //         <span>
-          //           <img src={verified || avatar4} alt="" className="pb-1" />
-          //         </span>
-          //       )}
-          //     </div> */}
-          //     <p className="">{ntf?.initiator_id?.display_name}</p>
-          //     <p className="text-primary-light-gray">{ntf?.content}</p>
-          //     <div className="mt-2">
-          //       <p className="mb-1">{ntf?.post_id?.content}</p>
+          <div
+            key={id}
+            className="flex gap-3 items-start bg-white border-b-2 p-5 w-full mt-4"
+          >
+            <div
+              className={`rounded-full border-4 w-[40px] h-[40px]`}
+              // style={{ borderColor: badgeColor || {post.user_id?.department[0]?.badge?.color} }}
+              style={{ borderColor: "gray" }}
+            >
+              <img
+                src={ntf?.initiator_id?.photo_url || avatar4}
+                className="rounded-full w-full h-full object-cover"
+                alt=""
+              />
+            </div>
+            <div className="flex-1">
+              {/* <div className="flex gap-2">
+                <p className="post-name pb-1">{fullname || "Eko Rugged"}</p>{" "}
+                {verifiedUser && (
+                  <span>
+                    <img src={verified || avatar4} alt="" className="pb-1" />
+                  </span>
+                )}
+              </div> */}
+              <p className="">{ntf?.initiator_id?.display_name}</p>
+              <p className="text-primary-light-gray">{ntf?.content}</p>
+              <div className="mt-2">
+                <p className="mb-1">{ntf?.post_id?.content}</p>
 
-          //       <div className="post-media rounded-md w-full">
-          //         <CustomCarousel
-          //           media_urls={ntf?.post_id?.media_urls}
-          //           left={left}
-          //           right={right}
-          //           dotsinactive={dotsinactive}
-          //           dotsactive={dotsactive}
-          //         />
-          //       </div>
-          //     </div>
-          //     <p className="text-primary-light-gray">
-          //       {getTimeAgoString(ntf?.updatedAt)}
-          //     </p>
-          //   </div>
-          //   <button>
-          //     <svg
-          //       xmlns="http://www.w3.org/2000/svg"
-          //       fill="none"
-          //       viewBox="0 0 24 24"
-          //       strokeWidth={1.5}
-          //       stroke="currentColor"
-          //       className="w-6 h-6"
-          //     >
-          //       <path
-          //         strokeLinecap="round"
-          //         strokeLinejoin="round"
-          //         d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
-          //       />
-          //     </svg>
-          //   </button>
-          // </div>
-          <div>hello</div>
+                <div className="post-media rounded-md w-full">
+                  <CustomCarousel
+                    media_urls={ntf?.post_id?.media_urls}
+                    left={left}
+                    right={right}
+                    dotsinactive={dotsinactive}
+                    dotsactive={dotsactive}
+                  />
+                </div>
+              </div>
+              <p className="text-primary-light-gray">
+                {getTimeAgoString(ntf?.updatedAt)}
+              </p>
+            </div>
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+                />
+              </svg>
+            </button>
+          </div>
+          // <div>hello</div>
         ))
       )}
     </MainLayout>
