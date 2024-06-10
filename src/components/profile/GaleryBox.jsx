8826107deefc13855759f00ba9d60f322306490f
JosephIgbaji <igbajijoseph.k@gmail.com>
2024-06-10 12:04:26 +0100

    linking token implemented

--- src/components/profile/GaleryBox.jsx
+++ src/components/profile/GaleryBox.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import Modals from "../modals/Modal";
+import MediaModal from "../main/MediaModal";
 
 const GaleryBox = ({ media }) => {
+  const [showMediaModal, setShowMediaModal] = useState(false);
+
   return (
-    <div className="w-[300px] h-[284px] rounded object-cover">
-      {/* <img
+    <>
+      <div className="w-[300px] h-[284px] rounded object-cover">
+        {/* <img
         className="object-cover w-[300px] h-[284px]"
         src={img}
         // src="https://images.pexels.com/photos/19175643/pexels-photo-19175643/free-photo-of-woman-in-coat-standing-by-wall-in-tunnel.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
@@ -11,46 +16,82 @@ const GaleryBox = ({ media }) => {
         // height={284}
         alt="personal medi a post"
       /> */}
-      {media.mimetype.startsWith("image") ? (
-        <img
-          src={media.downloadUrl}
-          className="object-cover w-[285px] h-[284px] rounded-lg"
-          alt="post image"
-        />
-      ) : (
-        <div className="flex items-center justify-center w-[285px] h-[284px] border-[1px] border-black rounded-lg bg-white">
-          <div className="flex items-center justify-center rounded-full h-[50px] w-[50px] bg-black">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="white"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
-              />
-            </svg>
+        {media.mimetype.startsWith("image") ? (
+          <img
+            src={media.downloadUrl}
+            onClick={() => setShowMediaModal(true)}
+            className="object-cover w-[285px] h-[284px] rounded-lg"
+            alt="post image"
+          />
+        ) : (
+          <div
+            onClick={() => setShowMediaModal(true)}
+            className="flex items-center justify-center w-[285px] h-[284px] border-[1px] border-black rounded-lg bg-white"
+          >
+            {/* <div className="flex items-center justify-center rounded-full h-[50px] w-[50px] bg-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="white"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
+                />
+              </svg>
+            </div> */}
+            <div className="w-full pb-5">
+              <video className="w-full" controls>
+                <source src={media.downlaodUrl} type="video/mp4" />
+                Sorry, your browser doesn't support embedded videos.
+              </video>
+            </div>
           </div>
-        </div>
-        // <video
-        //   className="object-cover w-[300px] h-[284px]"
-        //   controls
-        //   width="100%"
-        // >
-        //   <source src={media.downlaodUrl} type="video/mp4" />
-        //   Sorry, your browser doesn't support embedded videos.
-        // </video>
+        )}
+      </div>
+      {showMediaModal && (
+        <Modals
+          title={""}
+          openModal={showMediaModal}
+          modalSize="2xl"
+          onClose={() => setShowMediaModal(false)}
+        >
+          {/* <MediaModal media={media} /> */}
+          <div className="pt-4 post-wrapper max-h-[550px] w-full max-w-[491px] mx-auto">
+            <div className="post-card p-4">
+              <div className="flex items-center justify-between">
+                {media.mimetype.startsWith("image") ? (
+                  <div className="w-full">
+                    {" "}
+                    <img
+                      src={media.downloadUrl}
+                      className="object-cover"
+                      alt="post image"
+                    />
+                  </div>
+                ) : (
+                  <div className="w-full pb-5">
+                    <video className="w-full" autoPlay controls>
+                      <source src={media.downlaodUrl} type="video/mp4" />
+                      Sorry, your browser doesn't support embedded videos.
+                    </video>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </Modals>
       )}
-    </div>
+    </>
   );
 };
 
--- src/components/protected/ProtectedRoute.jsx
+++ src/components/protected/ProtectedRoute.jsx
@@ -5,27 +5,74 @@ import { LOGIN } from "../../routes/routes";
 import { useSearchParams } from "react-router-dom";
 import { updateUser } from "../../redux/slices/user.slice";
 import { useDispatch } from "react-redux";
+import { showAlert } from "../../static/alert";
+import axios from "axios";
+import { handleLogout } from "../../static/logout";
 
 const ProtectedRoute = ({ component: Component }) => {
   const [searchParams] = useSearchParams();
   const external_token = searchParams.get("token");
   const authtoken = useSelector((state) => state.user.token);
 
+  const apiUrl = import.meta.env.VITE_REACT_APP_BASE_URL;
+
   const dispatch = useDispatch();
   var isAuthenticated;
 
-  if (authtoken) {
+  // const dispatch = useDispatch();
+
+  // if (authtoken) {
+  //   isAuthenticated = authtoken !== null && authtoken !== undefined;
+  //   console.log(authtoken);
+  // } else if (external_token) {
+  //   dispatch(
+  //     updateUser({
+  //       token: external_token,
+  //       // user,
+  //     })
+  //   );
+  //   isAuthenticated = authtoken !== null && authtoken !== undefined;
+  //   console.log(external_token);
+  // }
+
+  const getNewUser = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/user/me`, {
+        headers: {
+          // "Content-Type": "multipart/form-data",
+          ...(external_token && { Authorization: `Bearer ${external_token}` }),
+        },
+      });
+
+      console.log("User gotten successfully:", response.data);
+      dispatch(
+        updateUser({
+          token: external_token,
+          user: response?.data?.data,
+        })
+      );
+
+      isAuthenticated = external_token !== null && external_token !== undefined;
+      console.log("external_token is available");
+
+      showAlert("Great!", "User retrieved successfully", "success");
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      showAlert(
+        "Oops!",
+        error?.response?.data?.message || "An error occurred",
+        "error"
+      );
+      handleLogout(dispatch);
+    }
+  };
+
+  if (external_token) {
+    getNewUser();
+    // console.log("External token: ", external_token);
+  } else if (authtoken) {
     isAuthenticated = authtoken !== null && authtoken !== undefined;
-    console.log(authtoken);
-  } else if (external_token) {
-    dispatch(
-      updateUser({
-        token: external_token,
-        // user,
-      })
-    );
-    isAuthenticated = authtoken !== null && authtoken !== undefined;
-    console.log(external_token);
+    // console.log(authtoken);
   }
 
   return <>{isAuthenticated ? <Component /> : <Navigate to={LOGIN} />}</>;
